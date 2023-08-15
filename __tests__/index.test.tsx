edd34a009eb52bd0e55744bac0bb5a3d06cd83f1
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-08-15 11:20:44 -0300

    feat: test ajustment

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -6,7 +6,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: /Bem vindo ao/i,
     })
 
     expect(heading).toBeInTheDocument()
